5d4a3b574d988b8724be81fdf38c767f12e83c98
Jeronimo <jeronimopenha@gmail.com>
2022-11-28 23:46:52 -0300

    exercícios lista 3

---
+++ Exercicios/2022_11_29/exe_10.py
@@ -0,0 +1,59 @@
+# minimum-sum descent forca bruta
+def msd_fb(p: list[list[int]], i: int, j: int) -> int:
+    if i == 0:
+        return p[i][j]
+    else:
+        r1 = msd_fb(p, i-1, j)
+        r2 = msd_fb(p, i-1, j-1)
+        r = min(r1, r2)
+        s = p[i][j] + r
+        return s
+
+# minimum-sum descent programacao dinamica
+
+
+def msd_pd(p: list[list[int]], i: int, j: int, tabela: list[list[int]]) -> int:
+    if i == 0:
+        return p[i][j]
+    else:
+        if tabela[i-1][j] is None:
+            r1 = msd_pd(p, i-1, j, tabela)
+            tabela[i-1][j] = r1
+        else:
+            r1 = tabela[i-1][j]
+        if tabela[i-1][j-1] is None:
+            r2 = msd_pd(p, i-1, j-1, tabela)
+            tabela[i-1][j-1] = r2
+        else:
+            r2 = tabela[i-1][j-1]
+        r = min(r1, r2)
+        s = p[i][j] + r
+        return s
+
+# minimum-sum descent guloso
+def msd_gl(p: list[list[int]], i: int, j: int) -> int:
+    if i == 0:
+        return p[i][j]
+    if (p[i-1][j]) < p[i-1][j-1]:
+        r = msd_gl(p, i-1, j)
+        s = p[i][j] + r
+        return s
+    else:
+        r = msd_gl(p, i-1, j-1)
+        s = p[i][j] + r
+        return s
+
+
+n = 4
+p = [
+    [6, 9, 6, 8],
+    [0, 7, 4, 1],
+    [0, 0, 4, 5],
+    [0, 0, 0, 2],
+
+
+]
+tabela = [[None for i in range(n)] for i in range(n)]
+i = 3
+j = 3
+print(msd_gl(p, i, j))
